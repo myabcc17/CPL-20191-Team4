539e6d9278076c28df0bad22909a1b32cad04fa4
mycool0905 <mycool0905@gmail.com>
2019-04-15 21:04:03 +0900

    Update Server to link to AWS

--- web2/server.js
+++ web2/server.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var app = express();
+var AWS = require('aws-sdk');
+AWS.config.region = 'ap-northeast-2';
+var ex2 = new AWS.EC2();
 var router = require('./router/main')(app);
 
 app.set('views', __dirname + '/webapp');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-var server = app.listen(3000, function(){
-    console.log("Express server has started on port 3000")
+var server = app.listen(8080, function () {
+    console.log("Express server has started on port 8080")
 });
 
 app.use(express.static('.'));
