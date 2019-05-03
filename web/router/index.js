0e4ef1095f40956e317a1fad5727355660b05142
myabcc17 <myabcc17@naver.com>
2019-05-04 01:52:04 +0900

    nodejs css 적용

--- web/router/index.js
+++ web/router/index.js
@@ -1,104 +1,102 @@
-module.exports = function(app)
-{
-	/*WebPage*/
-	app.get('/',function(req,res){
-                res.render('index.html');
+module.exports = function(app) {
+    /*WebPage*/
+    app.get('/', function(req, res) {
+        res.render('index.html');
+    });
+    app.get('/index.html', function(req, res) {
+        res.render('index.html');
+    });
+    app.get('/Finish.html', function(req, res) {
+        res.render('Finish.html');
+    });
+    app.get('/Register.html', function(req, res) {
+        res.render('Register.html');
+    });
+    app.get('/Important.html', function(req, res) {
+        res.render('Important.html');
+    });
+    app.get('/Recent.html', function(req, res) {
+        res.render('Recent.html');
+    });
+    app.get('/Trash.html', function(req, res) {
+        res.render('Trash.html');
+    });
+    app.get('/Login.html', function(req, res) {
+        res.render('Login.html');
+    });
+
+    /*DB*/
+    // GET ALL MEMBERS
+    app.get('/api/members', function(req, res) {
+        Member.find(function(err, members) {
+            if (err)
+                return res.status(500).send({ error: 'database failure' });
+            res.json(members);
         });
-	app.get('/index.html',function(req,res){
-		res.render('index.html');
-	});
-	app.get('/Finish.html',function(req,res){
-		res.render('Finish.html');
-	});
-	app.get('/Register.html',function(req,res){
-		res.render('Register.html');
-	});
-	app.get('/Important.html',function(req,res){
-                res.render('Important.html');
+    });
+    // GET SINGLE MEMBER
+    app.get('/api/members/:email', function(req, res) {
+        Member.findOne({ _email: req.params.email }, function(err, member) {
+            if (err)
+                return res.status(500).json({ error: err });
+            if (!member)
+                return res.status(404).json({ error: 'member not found' });
+            res.json(member);
         });
-	app.get('/Recent.html',function(req,res){
-                res.render('Recent.html');
+    });
+    // GET MEMBER BY NAME
+    app.get('/api/members/name/:name', function(req, res) {
+        Member.findOne({ _name: req.params.name }, function(err, book) {
+            if (err)
+                return res.status(500).json({ error: err });
+            if (!member)
+                return res.status(404).json({ error: 'member not found' });
+            res.json(member);
         });
-	app.get('/Trash.html',function(req,res){
-                res.render('Trash.html');
+    });
+
+    // CREATE MEMBER
+    app.post('/api/members', function(req, res) {
+        var member = new Member();
+        member.email = req.body.email;
+        member.password = req.body.password;
+        member.name = req.body.name;
+        member.phone = req.body.phone;
+        member.registered_date = new Date(req.body.registered_date);
+
+        member.save(function(err) {
+            if (err) {
+                console.error(err);
+                res.json({ result: 0 });
+                return;
+            }
+
+            res.json({ result: 1 });
         });
-	app.get('/Login.html',function(req,res){
-		res.render('Login.html');
-	});
+    });
 
-	/*DB*/
-	// GET ALL MEMBERS
-	app.get('/api/members',function(req,res){
-		Member.find(function(err, members){
-			if(err) 
-				return res.status(500).send({error: 'database failure'});
-			res.json(members);
-		});
-	});
-	// GET SINGLE MEMBER
-	app.get('/api/members/:email',function(req,res){
-		Member.findOne({_email: req.params.email}, function(err, member){
-			if(err)
-				return res.status(500).json({error: err});
-			if(!member)
-				return res.status(404).json({error: 'member not found'});
-			res.json(member);
-		});
-	});
-	// GET MEMBER BY NAME
-	app.get('/api/members/name/:name',function(req,res){
-		Member.findOne({_name: req.params.name}, function(err, book){
-			if(err)
-				return res.status(500).json({error: err});
-			if(!member)
-				return res.status(404).json({error: 'member not found'});
-			res.json(member);
-		});
-	});
+    // UPDATE THE MEBER
+    app.put('/api/members/:email', function(req, res) {
+        Member.findByEmail(req.params.email, function(err, member) {
+            if (err)
+                return res.status(500).json({ error: 'database failure' });
+            if (!member)
+                return res.status(404).json({ error: 'member not found' });
 
-	// CREATE MEMBER
-	app.post('/api/members',function(req,res){
-		var member = new Member();
-		member.email = req.body.email;
-		member.password = req.body.password;
-		member.name = req.body.name;
-		member.phone = req.body.phone;
-		member.registered_date = new Date(req.body.registered_date);
+            if (req.body.password) member.password = req.body.password;
+            if (req.body.name) member.name = req.body.name;
+            if (req.body.phone) member.phone = req.body.phone;
 
-		member.save(function(err){
-			if(err){
-				console.error(err);
-				res.json({result: 0});
-				return;
-			}
+            member.save(function(err) {
+                if (err)
+                    res.status(500).json({ error: 'failed to update' });
+                res.json({ message: 'member updated' });
+            });
+        });
+    });
 
-			res.json({result: 1});
-		});
-	});
-
-	// UPDATE THE MEBER
-	app.put('/api/members/:email',function(req,res){
-		Member.findByEmail(req.params.email, function(err, member){
-			if(err)
-				return res.status(500).json({error:'database failure'});
-			if(!member)
-				return res.status(404).json({error:'member not found'});
-
-			if(req.body.password) member.password = req.body.password;
-			if(req.body.name) member.name = req.body.name;
-			if(req.body.phone) member.phone = req.body.phone;
-
-			member.save(function(err){
-				if(err)
-					res.status(500).json({error:'failed to update'});
-				res.json({message:'member updated'});
-			});
-		});
-	});
-
-	// DELETE BOOK
-	app.delete('/api/members/:email',function(req,res){
-		res.end();
-	});
+    // DELETE BOOK
+    app.delete('/api/members/:email', function(req, res) {
+        res.end();
+    });
 }
-
--- web/server.js
+++ web/server.js
@@ -1,32 +1,39 @@
-var express	 = require('express');
-var app		 = express();
-var bodyParser	 = require('body-parser');
-var mongoose	 = require('mongoose');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+app.use(express.static(__dirname + '/css'));
+app.use(express.static(__dirname + '/js'));
+app.use(express.static(__dirname + '/fonts'));
+app.use(express.static(__dirname + '/model'));
+app.use(express.static(__dirname + '/vendor'));
+app.use(express.static(__dirname + '/webapp'));
 
 /*CONFIGURE APP TO USE bodyParser*/
-app.use(bodyParser.urlencoded({exteded: true }));
+app.use(bodyParser.urlencoded({ exteded: true }));
 app.use(bodyParser.json());
 
 /*CONFIGURE SERVER PORT*/
-var port	= process.env.PORT || 8080;
+var port = process.env.PORT || 8080;
 
 /*CONFIGURE ROUTER*/
-var router	= require('./router/')(app);
+var router = require('./router/')(app);
 
 app.set('views', __dirname + '/webapp');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-var server = app.listen(port, function () {
+var server = app.listen(port, function() {
     console.log("Express server has started on port " + port);
 });
 
 /*CONNECT TO MONGODB SERVER*/
 var db = mongoose.connection;
-db.on('error',console.error);
-db.once('open', function(){
-	//CONNECTED TO MONGODB SERVER
-	console.log("Connected to mongod server");
+db.on('error', console.error);
+db.once('open', function() {
+    //CONNECTED TO MONGODB SERVER
+    console.log("Connected to mongod server");
 });
 
 mongoose.connect('mongodb://localhost/PORTAI');
