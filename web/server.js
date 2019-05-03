adf9fd7fd149875200f54877890fc8203cb97559
myabcc17 <myabcc17@naver.com>
2019-05-03 16:09:59 +0000

    server, DB

--- web/server.js
+++ web/server.js
@@ -1,16 +1,34 @@
-var express = require('express');
-var app = express();
-//var AWS = require('aws-sdk');
-//AWS.config.region = 'ap-northeast-2';
-//var ex2 = new AWS.EC2();
-var router = require('./router/main')(app);
+var express	 = require('express');
+var app		 = express();
+var bodyParser	 = require('body-parser');
+var mongoose	 = require('mongoose');
+
+/*CONFIGURE APP TO USE bodyParser*/
+app.use(bodyParser.urlencoded({exteded: true }));
+app.use(bodyParser.json());
+
+/*CONFIGURE SERVER PORT*/
+var port	= process.env.PORT || 8080;
+
+/*CONFIGURE ROUTER*/
+var router	= require('./router/')(app);
 
 app.set('views', __dirname + '/webapp');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-var server = app.listen(8080, function () {
-    console.log("Express server has started on port 8080")
+var server = app.listen(port, function () {
+    console.log("Express server has started on port " + port);
 });
 
-app.use(express.static('.'));
+/*CONNECT TO MONGODB SERVER*/
+var db = mongoose.connection;
+db.on('error',console.error);
+db.once('open', function(){
+	//CONNECTED TO MONGODB SERVER
+	console.log("Connected to mongod server");
+});
+
+mongoose.connect('mongodb://localhost/PORTAI');
+
+var Member = require('./model/member');
