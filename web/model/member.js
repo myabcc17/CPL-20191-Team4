a15bd54237b579cb085362f544bfede5bdde30e8
unknown <mycool0905@gmail.com>
2019-06-01 19:55:16 +0900

    2019-06-01-19:55

--- web/model/member.js
+++ web/model/member.js
@@ -1,12 +1,16 @@
 var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
 
-var memberSchema = new Schema({
-    email: String,
-    password: String,
-    name: String,
+var memberSchema = mongoose.Schema({
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true, 'default' : '-1'},
+    name: {type: String, index : 'hashed'},
     phone: String,
-    //registered_date : { type : Date, default : Date.now }
+    registered_date : { type : Date, index : {unique : false}, 'default' : Date.now}
+});
+
+/* 스키마에 static 메소드 추가 */
+memberSchema.static('findByEmail', function(email, callback){
+    return this.find({email : email}, callback);
 });
 
 module.exports = mongoose.model('member', memberSchema);
